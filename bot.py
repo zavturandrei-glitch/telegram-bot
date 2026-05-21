6c8f93993e58980d039632be9e16a645e6c507b9
Andrei Zavtur <andrei@example.com>
2026-05-21 13:12:46 +0300

    remove traffic file

--- bot.py
+++ bot.py
@@ -14,12 +14,11 @@ from telegram.ext import (
     filters,
 )
 
-from traffic import morning_traffic
-
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.getenv("BOT_TOKEN")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+TOMTOM_API_KEY = os.getenv("TOMTOM_API_KEY")
 
 CHAT_ID = -1003817168180
 TIMEZONE = ZoneInfo("Europe/Chisinau")
@@ -45,12 +44,54 @@ def get_weather_text():
     )
 
 
+def get_traffic_text():
+    lat = 47.0105
+    lon = 28.8638
+
+    url = (
+        "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json"
+        f"?point={lat},{lon}&key={TOMTOM_API_KEY}"
+    )
+
+    response = requests.get(url)
+    data = response.json()
+
+    flow = data["flowSegmentData"]
+
+    current_speed = flow["currentSpeed"]
+    free_flow_speed = flow["freeFlowSpeed"]
+
+    if current_speed < free_flow_speed * 0.5:
+        status = "🔴 Сильные пробки"
+    elif current_speed < free_flow_speed * 0.8:
+        status = "🟠 Движение замедлено"
+    else:
+        status = "🟢 Движение свободное"
+
+    return (
+        "🚗 Пробки в Кишинёве\n\n"
+        f"Центр города: {status}\n"
+        f"Скорость сейчас: {current_speed} км/ч\n"
+        f"Обычно без пробок: {free_flow_speed} км/ч\n\n"
+        "Берегите время и планируйте маршрут заранее 🙌"
+    )
+
+
 async def morning_weather(context: ContextTypes.DEFAULT_TYPE):
     weather_text = get_weather_text()
 
     await context.bot.send_message(
         chat_id=CHAT_ID,
         text=weather_text
+    )
+
+
+async def morning_traffic(context: ContextTypes.DEFAULT_TYPE):
+    traffic_text = get_traffic_text()
+
+    await context.bot.send_message(
+        chat_id=CHAT_ID,
+        text=traffic_text
     )
 
 
@@ -91,12 +132,12 @@ app.add_handler(
 
 app.job_queue.run_daily(
     morning_weather,
-    time=time(hour=13, minute=5, tzinfo=TIMEZONE)
+    time=time(hour=13, minute=18, tzinfo=TIMEZONE)
 )
 
 app.job_queue.run_daily(
     morning_traffic,
-    time=time(hour=13, minute=6, tzinfo=TIMEZONE)
+    time=time(hour=13, minute=17, tzinfo=TIMEZONE)
 )
 
 app.run_polling()
